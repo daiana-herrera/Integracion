942df4c524c97f66031292988bbcdfb65065c002
daiana-herrera <daiana.herrera@accenture.com>
2022-12-14 08:57:18 -0300

    signuo

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -26,8 +26,8 @@ const usuariosGet = async(req = request, res = response) => {
 
 const usuariosPost = async(req, res = response) => {
     
-    const { nombre, correo, password, rol } = req.body;
-    const usuario = new Usuario({ nombre, correo, password, rol });
+    const {  correo, password } = req.body;
+    const usuario = new Usuario({  correo, password });
 
     // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
