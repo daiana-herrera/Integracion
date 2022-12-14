bbb3eaaca0ecfa522fb191d155b8c04530f6b84b
daiana-herrera <daiana.herrera@accenture.com>
2022-12-14 09:31:21 -0300

    signup

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -8,12 +8,12 @@ const {
 } = require('../middlewares');
 
 
-const {  emailExiste } = require('../helpers/db-validators');
+const {  emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const { usuariosGet,
-        
+        usuariosPut,
         usuariosPost,
-        
+        usuariosDelete,
         usuariosPatch } = require('../controllers/usuarios');
 
 const router = Router();
@@ -21,12 +21,25 @@ const router = Router();
 
 router.get('/', usuariosGet );
 
-router.post('/',[
+router.put('/:id',[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    validarCampos
+],usuariosPut );
+
+router.post('/signup',[
     check('password', 'El password debe de ser más de 6 letras').isLength({ min: 6 }),
     check('correo', 'El correo no es válido').isEmail(),
     check('correo').custom( emailExiste ),
     validarCampos
 ], usuariosPost );
+
+router.delete('/:id',[
+    validarJWT,
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    validarCampos
+],usuariosDelete );
 
 router.patch('/', usuariosPatch );
 
